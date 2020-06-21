c9280a9818d32c11f0adfb518c2cb7bf389a2790
davidkleiven <davidkleiven446@gmail.com>
2020-06-21 12:35:35 +0200

    ENH: Use gonum's iterative solvers instead (#4)

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/davidkleiven/gononlin
 
 go 1.13
+
+require (
+	gonum.org/v1/exp v0.0.0-20200309131419-fd4cb4db9971
+	gonum.org/v1/gonum v0.7.0
+)
--- nonlin/derivApprox.go
+++ nonlin/derivApprox.go
@@ -1,5 +1,7 @@
 package nonlin
 
+import "gonum.org/v1/gonum/mat"
+
 // VecFunc is a type that describes a vector valued function that should
 // be evaluated at point x. The result is placed in out.
 type VecFunc func(out []float64, x []float64)
@@ -7,33 +9,43 @@ type VecFunc func(out []float64, x []float64)
 // DerivativeApprox is a type that is used to approximate the dot product between
 // a search direction and the jacobian of a function
 type DerivativeApprox struct {
-	F       VecFunc
-	Eps     float64
-	Shifts  []float64
-	Weights []float64
+	// Function of from which the jacobian should be calculated
+	F VecFunc
+
+	// Stepsize used to calculate the inner product between the jacobian and a vector v
+	// J.dot(v) = (F(x + Eps*v) - F(x))/Eps
+	Eps float64
+
+	// X is the position at which the jacobian should be calculated
+	X       []float64
+	shifts  []float64
+	weights []float64
 }
 
-// Eval evaluates the dot product between the jacobian of the vector values function
+// MulVecTo evaluates the dot product between the jacobian of the vector values function
 // and a search direction v. x is the point at which the jacobian should be evaluated
 // and the result is given in out
-func (da *DerivativeApprox) Eval(x []float64, v []float64, out []float64) {
-	work := make([]float64, 2*len(x))
-	xn := work[:len(x)]
-	fVal := work[len(x):]
+func (da *DerivativeApprox) MulVecTo(dst *mat.VecDense, trans bool, v mat.Vector) {
+	if trans {
+		panic("DerivApprox: Transpose not supported")
+	}
+	work := make([]float64, 2*len(da.X))
+	xn := work[:len(da.X)]
+	fVal := work[len(da.X):]
 
 	// Make sure that out is cleared
-	for i := range out {
-		out[i] = 0.0
+	for i := range da.X {
+		dst.SetVec(i, 0.0)
 	}
 
-	for i := range da.Shifts {
-		for j := range x {
-			xn[j] = x[j] + da.Shifts[i]*v[j]*da.Eps
+	for i := range da.shifts {
+		for j := range da.X {
+			xn[j] = da.X[j] + da.shifts[i]*v.AtVec(j)*da.Eps
 		}
 		da.F(fVal, xn)
 
-		for j := range x {
-			out[j] += da.Weights[i] * fVal[j] / da.Eps
+		for j := range da.X {
+			dst.SetVec(j, dst.AtVec(j)+da.weights[i]*fVal[j]/da.Eps)
 		}
 	}
 }
@@ -45,8 +57,8 @@ func NewCentral(F VecFunc, Eps float64) DerivativeApprox {
 	return DerivativeApprox{
 		F:       F,
 		Eps:     Eps,
-		Shifts:  []float64{-1.0, 1.0},
-		Weights: []float64{-0.5, 0.5},
+		shifts:  []float64{-1.0, 1.0},
+		weights: []float64{-0.5, 0.5},
 	}
 }
 
@@ -58,8 +70,8 @@ func NewFourPoint(F VecFunc, Eps float64) DerivativeApprox {
 	return DerivativeApprox{
 		F:       F,
 		Eps:     Eps,
-		Shifts:  []float64{-2.0, -1.0, 1.0, 2.0},
-		Weights: []float64{1.0 / 12.0, -8.0 / 12.0, 8.0 / 12.0, -1.0 / 12.0},
+		shifts:  []float64{-2.0, -1.0, 1.0, 2.0},
+		weights: []float64{1.0 / 12.0, -8.0 / 12.0, 8.0 / 12.0, -1.0 / 12.0},
 	}
 }
 
@@ -72,8 +84,8 @@ func NewSixPoint(F VecFunc, Eps float64) DerivativeApprox {
 	return DerivativeApprox{
 		F:       F,
 		Eps:     Eps,
-		Shifts:  []float64{-3.0, -2.0, -1.0, 1.0, 2.0, 3.0},
-		Weights: []float64{-1.0 / 60.0, 9.0 / 60.0, -45.0 / 60.0, 45.0 / 60.0, -9.0 / 60.0, 1.0 / 60.0},
+		shifts:  []float64{-3.0, -2.0, -1.0, 1.0, 2.0, 3.0},
+		weights: []float64{-1.0 / 60.0, 9.0 / 60.0, -45.0 / 60.0, 45.0 / 60.0, -9.0 / 60.0, 1.0 / 60.0},
 	}
 }
 
@@ -86,7 +98,7 @@ func NewEightPoint(F VecFunc, Eps float64) DerivativeApprox {
 	return DerivativeApprox{
 		F:       F,
 		Eps:     Eps,
-		Shifts:  []float64{-4.0, -3.0, -2.0, -1.0, 1.0, 2.0, 3.0, 4.0},
-		Weights: []float64{3.0 / 840.0, -32.0 / 840.0, 168.0 / 840.0, -672.0 / 840.0, 672.0 / 840.0, -168.0 / 840.0, 32.0 / 840.0, -3.0 / 840.0},
+		shifts:  []float64{-4.0, -3.0, -2.0, -1.0, 1.0, 2.0, 3.0, 4.0},
+		weights: []float64{3.0 / 840.0, -32.0 / 840.0, 168.0 / 840.0, -672.0 / 840.0, 672.0 / 840.0, -168.0 / 840.0, 32.0 / 840.0, -3.0 / 840.0},
 	}
 }
